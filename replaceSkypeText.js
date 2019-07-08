f8bbf42b906914b27877d49ffbd6f399ab918e12
tony woode <tonywoodecode@gmail.com>
2019-07-08 19:42:51 +0100

    basic replaceSkypeText

---
+++ replaceSkypeText.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const replace = require('replace-in-file')
+const filenamePassed = process.argv.slice(2).toString()
+const inputFile = fs.readFileSync(filenamePassed, "utf8")
+
+const names = {
+
+  //close up peoples names where there's spaces and then a newline in between the name and what they say
+  from:  [ /^Keith.*\n/gm, /^Ari.*\n/gm, /^Steve.*\n/gm, /^Yannis.*\n/gm], 
+  to: [`keith: `, `tony: `, `ari: `, `steve: `, `yannis: `]
+}
+
+const myName = {
+
+  //skype doesn't print your own name, instead if prints just the time
+  from:  [ /^[0-9][0-9]:[0-9][0-9]\n/gm], 
+  to: [`tony: `]
+  //will match kwijibo (newline)I said some stuff and replace it with kwijibo: I said some stuff
+}
+   
+const blanklines = {
+
+  //tiddly needs blank lines for code quotes, but we don't have many of those in skype
+  //see the other scripts if you need >1 blank line instead, here let's just close up all blank lines
+  from: /^\s*[\r\n]/gm,
+  to: ``
+}
+
+const transform = (options) => {
+  //we're going to merge this files property into the options object to DRY it
+  const file = { files: filenamePassed }
+
+  try {
+    let changedFiles = replace.sync(Object.assign(options, file))
+    console.log(`replaced content in ${options.files}` )
+  }
+  catch (error) {
+    console.error(`Error occurred:`, error)
+  }
+
+}
+
+transform(names)
+transform(myName)
+transform(blanklines)
